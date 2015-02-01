3c69b5b091aa22fc19c5df834a1d4bc8a5606bc3
Frank Eslami <eslamif@oregonstate.edu>
2015-02-01 14:18:46 -0800

    WIP multiple page pull

--- script.js
+++ script.js
@@ -34,67 +34,64 @@ function searchGist() {
 	if(document.getElementsByName("text")[0].checked)
 		lanFilter.push("Text");			
 	
-	//Get Gists from Github
-	httpRequest.open("GET", "https://api.github.com/gists/public", true);
-	httpRequest.send();	
 	
-	//Store Gists from Github
-	httpRequest.onreadystatechange = function() {
-		if(httpRequest.readyState == 4) {
-			
-			//Store git results from Github
-			var resultAllParsed = JSON.parse(httpRequest.responseText);
-			
-
-			/*
-			//TEST TEST - lanFilter was successfully stored? YES YES
-			for(var i = 0; i < lanFilter.length; i++) {
-				console.log("lanFilter saved successfully? " + lanFilter[i]);
-			}
-			*/
-			
-			//Filter results by user language preferences
-			for(var i = 0; i < resultAllParsed.length; i++) {
-				for(var fileName in resultAllParsed[i].files) {								//files = object in resultAllParsed				
-					var oneFile = resultAllParsed[i].files[fileName];						//access properties of files object
-					for(var j = 0; j < lanFilter.length; j++) {								//filter based on user language preferences
-						if(lanFilter[j] == oneFile.language) {
-							resultDescription.push(resultAllParsed[i].description);			//save description
-							resultUrl.push(resultAllParsed[i].html_url);					//save url
-						}
-					}
-				}				
-			}
+	//Obtain Gists based on user's page number preference
+	var numPages = document.getElementById("pageNumber").value;									//user's page number
+	console.log("user page number = " + numPages);												//TEST - USER'S PAGE NUMBER INPUT
+	for(var pageNum = 1; pageNum < (numPages + 1); pageNum++) {
+		console.log("current page number = " + pageNum);										//TEST - CURRENT PAGE NUMBER ITERATION (SHOULD NOT EXCEED USER'S INPUT!)
+		//httpRequest.open("GET", "https://api.github.com/gists/public?page=pageNum", true);	//commented to prevent lockout
+		//httpRequest.send();																	//commented to prevent lockout
+		
+		//Store Gists from Github
+		httpRequest.onreadystatechange = function() {
+			if(httpRequest.readyState == 4) {
+				var resultAllParsed = JSON.parse(httpRequest.responseText);
 				
-			/*
-			//TEST TEST - filtered and saved correctly? YES YES
-			for(var i = 0; i < resultDescription.length; i++) {
-				console.log(resultDescription[i]);
-				console.log(resultUrl[i]);					
+				/*
+				//TEST TEST - lanFilter was successfully stored? YES YES
+				for(var i = 0; i < lanFilter.length; i++) {
+					console.log("lanFilter saved successfully? " + lanFilter[i]);
+				}
+				*/
+				
+				//Filter results by user language preferences
+				for(var i = 0; i < resultAllParsed.length; i++) {
+					for(var fileName in resultAllParsed[i].files) {								//files = object in resultAllParsed				
+						var oneFile = resultAllParsed[i].files[fileName];						//access properties of files object
+						for(var j = 0; j < lanFilter.length; j++) {								//filter based on user language preferences
+							if(lanFilter[j] == oneFile.language) {
+								resultDescription.push(resultAllParsed[i].description);			//save description
+								resultUrl.push(resultAllParsed[i].html_url);					//save url
+							}
+						}
+					}				
+				}
+					
+				/*
+				//TEST TEST - filtered and saved correctly? YES YES
+				for(var i = 0; i < resultDescription.length; i++) {
+					console.log(resultDescription[i]);
+					console.log(resultUrl[i]);					
+				}
+				*/
+
+				//Display Gist Results as list items	
+				var ul = document.getElementById('gistResults').appendChild(document.createElement("ul"));
+				var li;
+				
+				for(var i = 0; i < resultDescription.length; i++) {
+					li = document.createElement("li");
+					li.innerHTML = resultDescription[i];
+					ul.appendChild(li);	
+				}
+
+				/*
+				//Save to local storage
+				localStorage.setItem("test", resultAllParsed[0].description);
+				console.log(localStorage.getItem("test"));
+				*/
 			}
-			*/
-
-			//Display Gist Results as list items	
-			var ul = document.getElementById('gistResults').appendChild(document.createElement("ul"));
-			var li;
-			
-			for(var i = 0; i < resultDescription.length; i++) {
-				li = document.createElement("li");
-				li.innerHTML = resultDescription[i];
-				ul.appendChild(li);	
-			}
-
-			
-
-			/*
-			//Save to local storage
-			localStorage.setItem("test", resultAllParsed[0].description);
-			console.log(localStorage.getItem("test"));
-			*/
 		}
 	}
-	
-
-
-	
 }
