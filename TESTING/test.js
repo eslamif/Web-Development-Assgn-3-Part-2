746029846a928b6a741da50e28654b23014360c3
Frank Eslami <eslamif@oregonstate.edu>
2015-01-31 22:28:05 -0800

    Successfully listed gist pulls as list items

---
+++ TESTING/test.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+	
+	//document.getElementById('output').appendChild(document.createElement("ul"));
+	
+	var ul = document.createElement("ul");	
+	document.getElementById('output').appendChild(ul);
+	
+	/*
+	var ul = document.createElement("ul");
+	var li = document.createElement("li");
+	ul.appendChild(li);
+	
+	document.getElementById('output').appendChild(ul);
+	*/
+	
+	//document.getElementById('output').appendChild(li);
+	//document.getElementById('output').innerHTML = '<div></div>';
+}
--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-window.onload = function() {
-	getGists();
+window.onload = function(){
 }
 
-//Ajax request for Gists from Github
-var httpRequest;
-if(window.XMLHttpRequest) {					//Browsers other than internet explorer
-	httpRequest = new XMLHttpRequest();
-}
-else if(window.ActiveXObject) {				//internet explore browser
-	httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-}
-if(!req) {
-	throw 'Unable to create HttpRequest.';
+function searchGist() {
+	var httpRequest;
+	if(window.XMLHttpRequest) {					//Browsers other than internet explorer
+		httpRequest = new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject) {				//internet explore browser
+		httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	if(!httpRequest) {
+		throw 'Unable to create HttpRequest.';
+	}	
+
+	httpRequest.onreadystatechange = function() {
+		if(httpRequest.readyState == 4) {
+			//Store git results from Github
+			var resultAllParsed = JSON.parse(httpRequest.responseText);
+			var resultDescription = [];
+			var resultHtmlUrl = [];
+			
+			for(var i = 0; i < resultAllParsed.length; i++) {
+				resultDescription[i] = resultAllParsed[i].description;
+				resultHtmlUrl[i] = resultAllParsed[i].html_url;
+			}
+			
+			//Display Gist Results as list items	
+			var ul = document.getElementById('gistResults').appendChild(document.createElement("ul"));
+			var li;
+			
+			for(var i = 0; i < resultDescription.length; i++) {
+				li = document.createElement("li");
+				li.innerHTML = resultDescription[i];
+				ul.appendChild(li);	
+			}
+
+			
+
+			/*
+			//Save to local storage
+			localStorage.setItem("test", resultAllParsed[0].description);
+			console.log(localStorage.getItem("test"));
+			*/
+		}
+	}
+	
+	httpRequest.open("GET", "https://api.github.com/gists/public", true);
+	httpRequest.send();
+
+	
 }
