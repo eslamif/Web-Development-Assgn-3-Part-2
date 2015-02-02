b0632b8e8d7d1cd49a6d89519eefb721fde61285
Frank Eslami <eslamif@oregonstate.edu>
2015-02-01 17:07:18 -0800

    Successfully pulls multiple pages. Work on displaying them next

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onload = function(){
 }
 
+function initVars() {
+	//Global variables
+	lanFilter = []
+	resultDescription = [];
+	resultUrl = [];
+	userPageInput = document.getElementById("pageNumber").value;
+	
+	for(pages = 0; pages < userPageInput; pages++) {
+		searchGist();
+	}	
+}
+
 function searchGist() {
 	var httpRequest;
 	if(window.XMLHttpRequest) {					//Browsers other than internet explorer
@@ -13,10 +25,6 @@ function searchGist() {
 	if(!httpRequest) {
 		throw 'Unable to create HttpRequest.';
 	}	
-
-	var lanFilter = []
-	var resultDescription = [];
-	var resultUrl = [];
 
 	//Determine which language box the user checked
 	if(document.getElementsByName("python")[0].checked)
@@ -36,62 +44,61 @@ function searchGist() {
 	
 	
 	//Obtain Gists based on user's page number preference
-	var numPages = document.getElementById("pageNumber").value;									//user's page number
-	console.log("user page number = " + numPages);												//TEST - USER'S PAGE NUMBER INPUT
-	for(var pageNum = 1; pageNum < (parseInt(numPages, 10) + 1); pageNum++) {
-		console.log("current page number = " + pageNum);										//TEST - CURRENT PAGE NUMBER ITERATION (SHOULD NOT EXCEED USER'S INPUT!)
-		httpRequest.open("GET", "https://api.github.com/gists/public?page=pageNum", true);	//commented to prevent lockout
-		httpRequest.send();																	//commented to prevent lockout
-		
-		//Store Gists from Github
-		httpRequest.onreadystatechange = function() {
-			if(httpRequest.readyState == 4) {
-				var resultAllParsed = JSON.parse(httpRequest.responseText);
-				
-				/*
-				//TEST TEST - lanFilter was successfully stored? YES YES
-				for(var i = 0; i < lanFilter.length; i++) {
-					console.log("lanFilter saved successfully? " + lanFilter[i]);
-				}
-				*/
-				
-				//Filter results by user language preferences
-				for(var i = 0; i < resultAllParsed.length; i++) {
-					for(var fileName in resultAllParsed[i].files) {								//files = object in resultAllParsed				
-						var oneFile = resultAllParsed[i].files[fileName];						//access properties of files object
-						for(var j = 0; j < lanFilter.length; j++) {								//filter based on user language preferences
-							if(lanFilter[j] == oneFile.language) {
-								resultDescription.push(resultAllParsed[i].description);			//save description
-								resultUrl.push(resultAllParsed[i].html_url);					//save url
-							}
-						}
-					}				
-				}
-					
-				/*
-				//TEST TEST - filtered and saved correctly? YES YES
-				for(var i = 0; i < resultDescription.length; i++) {
-					console.log(resultDescription[i]);
-					console.log(resultUrl[i]);					
-				}
-				*/
-
-				//Display Gist Results as list items	
-				var ul = document.getElementById('gistResults').appendChild(document.createElement("ul"));
-				var li;
-				
-				for(var i = 0; i < resultDescription.length; i++) {
-					li = document.createElement("li");
-					li.innerHTML = resultDescription[i];
-					ul.appendChild(li);	
-				}
-
-				/*
-				//Save to local storage
-				localStorage.setItem("test", resultAllParsed[0].description);
-				console.log(localStorage.getItem("test"));
-				*/
+	//console.log("user page number = " + numPages);										//TEST - USER'S PAGE NUMBER INPUT
+	//console.log("current page number = " + pageNum);										//TEST - CURRENT PAGE NUMBER ITERATION (SHOULD NOT EXCEED USER'S INPUT!)
+	var gistPage = pages + 1;
+	httpRequest.open("GET", "https://api.github.com/gists/public?page=gistPage", true);	//commented to prevent lockout
+	httpRequest.send();																	//commented to prevent lockout
+	
+	//Store Gists from Github
+	httpRequest.onreadystatechange = function() {
+		if(httpRequest.readyState == 4) {
+			var resultAllParsed = JSON.parse(httpRequest.responseText);
+			
+			/*
+			//TEST TEST - lanFilter was successfully stored? YES YES
+			for(var i = 0; i < lanFilter.length; i++) {
+				console.log("lanFilter saved successfully? " + lanFilter[i]);
 			}
+			*/
+			
+			//Filter results by user language preferences
+			for(var i = 0; i < resultAllParsed.length; i++) {
+				for(var fileName in resultAllParsed[i].files) {								//files = object in resultAllParsed				
+					var oneFile = resultAllParsed[i].files[fileName];						//access properties of files object
+					for(var j = 0; j < lanFilter.length; j++) {								//filter based on user language preferences
+						if(lanFilter[j] == oneFile.language) {
+							resultDescription.push(resultAllParsed[i].description);			//save description
+							resultUrl.push(resultAllParsed[i].html_url);					//save url
+						}
+					}
+				}				
+			}
+				
+			/*
+			//TEST TEST - filtered and saved correctly? YES YES
+			for(var i = 0; i < resultDescription.length; i++) {
+				console.log(resultDescription[i]);
+				console.log(resultUrl[i]);					
+			}
+			*/
+
+			//Display Gist Results as list items	
+			var ul = document.getElementById('gistResults').appendChild(document.createElement("ul"));
+			var li;
+			
+			for(var i = 0; i < resultDescription.length; i++) {
+				li = document.createElement("li");
+				li.innerHTML = resultDescription[i];
+				ul.appendChild(li);	
+			}
+
+			/*
+			//Save to local storage
+			localStorage.setItem("test", resultAllParsed[0].description);
+			console.log(localStorage.getItem("test"));
+			*/
 		}
 	}
+
 }
